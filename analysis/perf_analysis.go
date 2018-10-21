0a668678fa81666e3a7731ca3b99d378b5394f4b
Kevis Tsao <kevis.tsao@vanderbilt.edu>
2018-10-21 01:15:12 -0400

    Finished work on apicaller, still issue with hardcoded summID but work on analysis being finalized.

---
+++ analysis/perf_analysis.go
@@ -0,0 +1,10 @@
+package analysis
+
+type TiltFactors struct {
+
+}
+
+
+//func Performance(matches riotapi.MatchListDto) int {
+//	//Factors for it
+//}
--- riotapi/apicaller.go
+++ riotapi/apicaller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"io/ioutil"
 	"encoding/json"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 type SummonerDto struct {
@@ -34,27 +36,19 @@ type MatchListDto struct {
 }
 
 // For out of testing use, when not using a temp dev key.
-//var (
-//	apiKey = os.Getenv("RIOTAPIKEY")
-//)
 
 func MatchListCaller(sumName string) MatchListDto {
 	//Gets SummonerID from Summoner Name
-	fmt.Println(sumName)
-	//"https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Starfirephoenix?api_key=RGAPI-c438d1a9-cfbe-4fa4-811f-98a8988c494e"
-	//fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s", sumName, apiKey)
-	//var apiKey = ""
-	//var url = fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s", sumName, apiKey)
-	request, _ := http.NewRequest("GET", fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s", sumName), nil)
-	//request.Header.Set("X-Riot-Token", apiKey)
-	//request.Header.Set("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8")
-	//request.Header.Set("Origin", "https://developer.riotgames.com")
-	//request.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	//request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	apiKey := os.Getenv("RIOT_API_KEY")
+	url := "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Starfirephoenix"
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("X-Riot-Token", apiKey)
 
 	client := &http.Client{}
-	//var url = strings.Join([]string{"https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/", sumName, "?api_key=", apiKey}, "")
-	fmt.Println(fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Starfirephoenix"))
 	res, err := client.Do(request)
 	if err != nil {
 		log.Fatalf("Bad HTTP Request with error: %s.", err)
